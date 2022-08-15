8cd8c27d6ed804dec1d6b5ccf0afab5486873ad9
Takuto Kojima <mail@takutokojima.com>
2022-08-15 18:38:07 +0200

    implemented auto creation of review pending revision

--- helpers/FidusWriterRevisionHelper.inc.php
+++ helpers/FidusWriterRevisionHelper.inc.php
@@ -23,7 +23,41 @@ class FidusWriterRevisionHelper {
 			$stageId = intval($submission->getData('stageId'));
 			$plugin = FidusWriterPluginHelper::getFidusWriterPlugin();
 
-			if (4 > $stageId && SUBMISSION_EDITOR_DECISION_ACCEPT == $decision) {
+			if (3 === $stageId && SUBMISSION_EDITOR_DECISION_PENDING_REVISIONS == $decision) {
+				// Request Revisions
+				$reviewRoundDao = DAORegistry::getDAO('ReviewRoundDAO');
+				$reviewRound = $reviewRoundDao->getLastReviewRoundBySubmissionId($submission->getId(), $stageId);
+
+				if ($reviewRound instanceof ReviewRound) {
+					// Check if review revision for rework (3.x.5) already exists
+					$plugin->import('dao.FidusWriterReviewRoundRevisionDAO');
+					$reviewRoundRevisionDao = new FidusWriterReviewRoundRevisionDAO();
+					$reviewRoundRevision = $reviewRoundRevisionDao->getRoundRevision($reviewRound->getId());
+					if (
+						!$reviewRoundRevision instanceof DataObject ||
+						empty($reviewRoundRevision->getData('revision_url'))
+					) {
+						// no revision for rework found, so create rework revision in FW
+						$oldVersionString = FidusWriterPluginHelper::stageToVersion($stageId, $reviewRound->getRound(), 'Reviewer');
+						$newVersionString = FidusWriterPluginHelper::stageToVersion($stageId, $reviewRound->getRound(), 'Author');
+						// Assigned users
+						$assiendUsers = FidusWriterPluginHelper::getAssignedUserIds($submission->getId(), $reviewRound->getStageId());
+
+						// Insert reviewRoundRevision
+						/**
+						 * @var FidusWriterReviewRoundRevisionDAO $reviewRoundRevisionDao
+						 * @var DataObject $reviewRoundRevision
+						 */
+						$reviewRoundRevisionDao = DAORegistry::getDAO('FidusWriterReviewRoundRevisionDAO');
+						// Get URL to revision in FidusWriter
+						$revisionUrl = FidusWriterPluginHelper::getGatewayPluginUrl() . '/documentReview?submissionId=' . $submission->getId() . '&stageId=' . $reviewRound->getStageId() . '&version=' . $newVersionString;
+						$reviewRoundRevision = new DataObject();
+						$reviewRoundRevision->setData('review_round', $reviewRound->getId());
+						$reviewRoundRevision->setData('revision_url', $revisionUrl);
+						$reviewRoundRevisionDao->save($reviewRoundRevision);
+					}
+				}
+			} elseif (4 > $stageId && SUBMISSION_EDITOR_DECISION_ACCEPT == $decision) {
 				// Submission sent to copyediting from a previous step
 				$oldRound = 0;
 				$oldRevisionType = "Reviewer";
